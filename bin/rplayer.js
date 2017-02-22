87ca2e3a140eda1df8020f5ecc660f5fd1480cf8
jenkins <dokoto.moloko@gmail.com>
2017-02-22 16:46:25 +0100

    Developing..

--- bin/rplayer.js
+++ bin/rplayer.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
+
 'use strict';
 
+const Options = require('../lib/options.js');
 const RPlayer = require('../lib/rplayerlib.js');
 
-new RPlayer().run();
+class Main {
+    constructor() {
+        this.rplayer = new RPlayer();
+        this.options = new Options(process.argv);
+    }
+
+    run() {
+        try {
+            this.options.checkDependencies();
+            let options = this.options.get();
+            if (options.help) {
+                this.options.help();
+            } else {
+                this.rplayer.run(options);
+            }
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+}
+
+new Main().run();
